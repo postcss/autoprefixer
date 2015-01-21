38c1f06f914e127ed0e83fc84963bcf2f1be028e
Andrey Sitnik <andrey@sitnik.ru>
2015-01-21 14:57:56 +0300

    Fix help message

--- binary.js
+++ binary.js
@@ -33,10 +33,10 @@ Options:
   -d, --dir DIR            set output dir
   -m, --map                generate source map
       --no-map             skip source map even if previous map exists
-      --no-inline          do not inline maps to data:uri
+      --no-inline-map      do not inline maps to data:uri
       --inline-map         force inline map
       --annotation PATH    change map location relative from CSS file
-      --no-map-annotation  skip source map annotation comment is CSS
+      --no-map-annotation  do not add source map annotation comment in CSS
       --no-sources-content remove origin CSS from maps
       --sources-content    force include origin CSS into map
       --no-cascade         do not create nice visual cascade of prefixes
