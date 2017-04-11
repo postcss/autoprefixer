824e64c86513081a6798025d56559c92687f89ca
Andrey Sitnik <andrey@sitnik.ru>
2017-04-11 13:09:05 +1200

    Fix test/utils.js

--- test/utils.js
+++ test/utils.js
@@ -4,9 +4,13 @@ describe('utils', () => {
 
     describe('.error()', () => {
 
-        it('raises an error', () => ( () => utils.error('A')).should.throw('A'));
+        it('raises an error', () => {
+            (() => {
+                utils.error('A');
+            }).should.throw('A');
+        });
 
-        return it('marks an error', () => {
+        it('marks an error', () => {
             let error = null;
             try {
                 utils.error('A');
@@ -14,30 +18,36 @@ describe('utils', () => {
                 error = e;
             }
 
-            return error.autoprefixer.should.be.true;
+            error.autoprefixer.should.be.true;
         });
+
     });
 
-    describe('.uniq()', () =>
+    describe('.uniq()', () => {
 
-    it('filters doubles in array', () => utils.uniq(['1', '1', '2', '3', '3']).should.eql(['1', '2', '3']))
-);
+        it('filters doubles in array', () => {
+            utils.uniq(['1', '1', '2', '3', '3']).should.eql(['1', '2', '3']);
+        });
 
-    describe('.removeNote()', () =>
+    });
 
-    it('removes note', () => {
-        utils.removeNote('-webkit- note').should.eql('-webkit-');
-        return utils.removeNote('-webkit-').should.eql('-webkit-');
-    })
-  );
+    describe('.removeNote()', () => {
 
-    describe('.escapeRegexp()', () =>
+        it('removes note', () => {
+            utils.removeNote('-webkit- note').should.eql('-webkit-');
+            utils.removeNote('-webkit-').should.eql('-webkit-');
+        });
 
-    it('escapes RegExp symbols', () => {
-        const string = utils.escapeRegexp('^[()\\]');
-        return string.should.eql('\\^\\[\\(\\)\\\\\\]');
-    })
-  );
+    });
+
+    describe('.escapeRegexp()', () => {
+
+        it('escapes RegExp symbols', () => {
+            const string = utils.escapeRegexp('^[()\\]');
+            string.should.eql('\\^\\[\\(\\)\\\\\\]');
+        });
+
+    });
 
     describe('.regexp()', () => {
 
@@ -50,14 +60,15 @@ describe('utils', () => {
             test('one, foo, two').should.be.ok;
             test('one(),foo(),two()').should.be.ok;
 
-            'foo(), a, foo'.replace(regexp, '$1b$2').should.eql('bfoo(), a, bfoo');
+            'foo(), a, foo'.replace(regexp, '$1b$2')
+                .should.eql('bfoo(), a, bfoo');
 
             test('foob').should.be.false;
             test('(foo)').should.be.false;
-            return test('-a-foo').should.be.false;
+            test('-a-foo').should.be.false;
         });
 
-        return it('escapes string if needed', () => {
+        it('escapes string if needed', () => {
             let regexp = utils.regexp('(a|b)');
             const test   = string => string.match(regexp) !== null;
 
@@ -66,15 +77,16 @@ describe('utils', () => {
 
             regexp = utils.regexp('(a|b)', false);
             test('a').should.be.ok;
-            return test('b').should.be.ok;
+            test('b').should.be.ok;
         });
+
     });
 
-    return describe('.editList()', () => {
+    describe('.editList()', () => {
 
         it('does save without changes', () => {
-            const list = utils.editList('a,\nb, c', (parsed, edit) => parsed);
-            return list.should.eql('a,\nb, c');
+            const list = utils.editList('a,\nb, c', parsed => parsed);
+            list.should.eql('a,\nb, c');
         });
 
         it('changes list', () => {
@@ -83,17 +95,19 @@ describe('utils', () => {
                 edit.should.eql([]);
                 return ['1', '2'];
             });
-            return list.should.eql('1, 2');
+            list.should.eql('1, 2');
         });
 
         it('saves comma', () => {
-            const list = utils.editList('a,\nb', (parsed, edit) => ['1', '2']);
-            return list.should.eql('1,\n2');
+            const list = utils.editList('a,\nb', () => ['1', '2']);
+            list.should.eql('1,\n2');
         });
 
-        return it('parse one value', () => {
-            const list = utils.editList('1', (parsed, edit) => [parsed[0], '2']);
-            return list.should.eql('1, 2');
+        it('parse one value', () => {
+            const list = utils.editList('1', (parsed) => [parsed[0], '2']);
+            list.should.eql('1, 2');
         });
+
     });
+
 });
