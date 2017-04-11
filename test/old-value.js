210912e11b9b5960e44d41e70e523b4d0709c0aa
Andrey Sitnik <andrey@sitnik.ru>
2017-04-11 13:24:01 +1200

    Fix test/old-value.js

--- test/old-value.js
+++ test/old-value.js
@@ -1,18 +1,20 @@
 const OldValue = require('../lib/old-value');
 
-describe('OldValue', () =>
+describe('OldValue', () => {
 
-  describe('.check()', () => {
+    describe('.check()', () => {
 
-      it('checks value in string', () => {
-          const old = new OldValue('calc', '-o-calc');
-          old.check('1px -o-calc(1px)').should.be.true;
-          return old.check('1px calc(1px)').should.be.false;
-      });
+        it('checks value in string', () => {
+            const old = new OldValue('calc', '-o-calc');
+            old.check('1px -o-calc(1px)').should.be.true;
+            old.check('1px calc(1px)').should.be.false;
+        });
 
-      return it('allows custom checks', () => {
-          const old = new OldValue('calc', '-o-calc', 'calc', /calc/);
-          return old.check('1px calc(1px)').should.be.true;
-      });
-  })
-);
+        it('allows custom checks', () => {
+            const old = new OldValue('calc', '-o-calc', 'calc', /calc/);
+            return old.check('1px calc(1px)').should.be.true;
+        });
+
+    });
+
+});
