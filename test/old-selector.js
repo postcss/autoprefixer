bdd3cca59938d4cb09bff791930815089127ddc8
Andrey Sitnik <andrey@sitnik.ru>
2017-04-11 13:23:05 +1200

    Fix test/old-selector.js

--- test/old-selector.js
+++ test/old-selector.js
@@ -1,46 +1,51 @@
-const OldSelector = require('../lib/old-selector');
-const Selector    = require('../lib/selector');
-const parse       = require('postcss/lib/parse');
+const Selector = require('../lib/selector');
+const parse    = require('postcss/lib/parse');
 
 describe('OldSelector', () => {
+
     beforeEach(function () {
         const selector = new Selector('::selection', ['-moz-', '-ms-']);
-        return this.old     = selector.old('-moz-');
+        this.old = selector.old('-moz-');
     });
 
     describe('isHack()', () => {
 
         it('returns true on last rule', function () {
             const css = parse('::selection {} ::-moz-selection {}');
-            return this.old.isHack(css.last).should.be.true;
+            this.old.isHack(css.last).should.be.true;
         });
 
         it('stops on another type', function () {
-            const css = parse('::-moz-selection {} @keyframes anim {} ::selection {}');
-            return this.old.isHack(css.first).should.be.true;
+            const css = parse('::-moz-selection {} ' +
+                '@keyframes anim {} ::selection {}');
+            this.old.isHack(css.first).should.be.true;
         });
 
         it('stops on another selector', function () {
             const css = parse('::-moz-selection {} a {} ::selection {}');
-            return this.old.isHack(css.first).should.be.true;
+            this.old.isHack(css.first).should.be.true;
         });
 
-        return it('finds unprefixed selector', function () {
-            const css = parse('::-moz-selection {} ::-o-selection {} ::selection {}');
-            return this.old.isHack(css.first).should.be.false;
+        it('finds unprefixed selector', function () {
+            const css = parse('::-moz-selection {} ' +
+                '::-o-selection {} ::selection {}');
+            this.old.isHack(css.first).should.be.false;
         });
+
     });
 
-    return describe('check()', () => {
+    describe('check()', () => {
 
         it('finds old selector', function () {
             const css = parse('body::-moz-selection {} body::selection {}');
-            return this.old.check(css.first).should.be.true;
+            this.old.check(css.first).should.be.true;
         });
 
-        return it('finds right', function () {
+        it('finds right', function () {
             const css = parse('body:::-moz-selection {}');
-            return this.old.check(css.first).should.be.false;
+            this.old.check(css.first).should.be.false;
         });
+
     });
+
 });
