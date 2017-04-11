9c42a4b8be3e614b79b34fe8ba681280a997c6d4
Andrey Sitnik <andrey@sitnik.ru>
2017-04-11 13:12:54 +1200

    Fix test/brackets.js

--- test/brackets.js
+++ test/brackets.js
@@ -4,21 +4,40 @@ describe('brackets', () => {
 
     describe('.parse()', () => {
 
-        it('parses simple string', () => brackets.parse('test').should.eql(['test']));
+        it('parses simple string', () => {
+            brackets.parse('test').should.eql(['test']);
+        });
 
-        it('parses brackets', () => brackets.parse('a (b) a').should.eql(['a ', ['b'], ' a']));
+        it('parses brackets', () => {
+            brackets.parse('a (b) a').should.eql(['a ', ['b'], ' a']);
+        });
 
-        it('parses many brackets', () => brackets.parse('a (b ()) a').should.eql(['a ', ['b ', [''], ''], ' a']));
+        it('parses many brackets', () => {
+            brackets.parse('a (b ()) a')
+                .should.eql(['a ', ['b ', [''], ''], ' a']);
+        });
 
-        return it('parses errors', () => brackets.parse('a (b (').should.eql(['a ', ['b ', ['']]]));
+        it('parses errors', () => {
+            brackets.parse('a (b (').should.eql(['a ', ['b ', ['']]]);
+        });
+
     });
 
     return describe('.stringify()', () => {
 
-        it('stringifies simple string', () => brackets.stringify(['test']).should.eql('test'));
+        it('stringifies simple string', () => {
+            brackets.stringify(['test']).should.eql('test');
+        });
 
-        it('stringifies brackets', () => brackets.stringify(['a ', ['b'], ' a']).should.eql('a (b) a'));
+        it('stringifies brackets', () => {
+            brackets.stringify(['a ', ['b'], ' a']).should.eql('a (b) a');
+        });
 
-        return it('stringifies many brackets', () => brackets.stringify(['a ', ['b ', [''], ''], ' a']).should.eql('a (b ()) a'));
+        it('stringifies many brackets', () => {
+            brackets.stringify(['a ', ['b ', [''], ''], ' a'])
+                .should.eql('a (b ()) a');
+        });
+
     });
+
 });
